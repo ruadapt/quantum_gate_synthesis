bcc1805dd817f899c6127ab0f92112fae666d641
Jonathan-Ackerman <jonathanackerman01@gmail.com>
2023-01-28 21:08:55 -0500

    Add command-line arguments to main

--- main.cpp
+++ main.cpp
@@ -2,17 +2,25 @@
 #include "gridSynth.h"
 #include "multiQubitSynthesis.h"
 #include "cliffordT.h"
+#include "matrix.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    Real prec = 5;
-    Real theta = 1.7;
-    int effort = 25;
+    if (argc < 3)
+    {
+        std::cout << "Usage: ./main <angle in radians> <precision in bits>" << std::endl;
+        return 1;
+    }
+    char *prec_str = argv[2];
+    Real prec(prec_str);
+    char *theta_str = argv[1];
+    Real theta(theta_str);
+    int effort = 25; // TODO make this configurable?
     U2<DOmega> uU;
     Maybe<double> error;
     List<std::tuple<DOmega, Integer, gridsynth::DStatus>> candidate_info;
     std::tie(uU, error, candidate_info) = gridsynth::gridsynth_internal<Real>(prec, theta, effort);
     List<Gate> gates = clifford_t::synthesis_u2(uU);
-    std::cout << "gates:" << std::endl;
-    std::cout << clifford_t::to_string(gates) << std::endl;
+    std::cout << "Gates: " << clifford_t::to_string(gates) << std::endl;
+    std::cout << "Error: " << error.value_or(0) << std::endl;
 }
